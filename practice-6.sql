cd79d72bf41980d0a8552d3697ca09288845647d
terrygoguen <terry.goguen@kellogg.northwestern.edu>
2023-01-17 04:00:38 +0000

    practice hw updated

--- practice-6.sql
+++ practice-6.sql
@@ -15,4 +15,7 @@
 -- | Brighton Park          | 678      |
 -- | Burnside               | 10       |
 
-
+SELECT neighborhood, COUNT(neighborhood)
+FROM reviews 
+INNER JOIN listings ON listings.id = reviews.listing_id
+GROUP BY neighborhood;
--- practice-7.sql
+++ practice-7.sql
@@ -16,4 +16,7 @@
 -- | Entire townhouse                    | 3588     | 2021-10-17                 |
 -- | Entire villa                        | 75       | 2021-10-12                 |
 
-
+SELECT property_type, COUNT(property_type), date_reviewed
+FROM reviews 
+INNER JOIN listings ON listings.id = reviews.listing_id
+GROUP BY property_type;
